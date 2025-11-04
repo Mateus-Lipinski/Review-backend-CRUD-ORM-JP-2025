b2c8380e113b4e4314fa38c183962b3e1b1e988d
Mateus Lipinski <mateuslipinskimatias@gmail.com>
2025-11-04 19:44:07 -0300

    finish/controller/services

--- src/service/produto.js
+++ src/service/produto.js
@@ -8,18 +8,59 @@ class ServiceProduto {
     }
 
     async PegarUm(id) {
+        
+        if(!id) {
+            throw new Error("Favor informar o ID!")
+        }
         const produto = await Produto.findByPk(id)
+
+        if(!produto) {
+            throw new Error(`Usuário ${id} não foi encontrado!`)
+        }
+
         return produto
     }
     
     async Criar(nome, disponivel, quantidade) {
+
+        if(!nome || !disponivel || !quantidade) {
+            throw new Error("Favor preencher os campos!")
+        }
+
         await Produto.create({ nome, disponivel, quantidade })
     }
     
-    async Alterar(id, nome, disponivel, quantidade) {}
+    async Alterar(id, nome, disponivel, quantidade) {
+
+        if(!id || !nome || !disponivel || !quantidade) {
+            throw new Error("Favor preencher os campos!")
+        }
+
+        const produto = await Produto.findByPk(id)
+
+        if(!produto) {
+            throw new Error(`Usuário ${id} não foi encontrado!`)
+        }
+    
+        produto.nome = nome
+        produto.disponivel = disponivel
+        produto.quantidade = quantidade
+        
+        await produto.save()
+    }
     
     async Deletar(id) {
-        await Produto.destroy(id)
+
+        if(!id) {
+            throw new Error("Favor informar o ID!")
+        }
+        const produto = await Produto.findByPk(id)
+
+        if(!produto) {
+            throw new Error(`Usuário ${id} não foi encontrado!`)
+        }
+
+        await produto.destroy()
     }
 }
 
